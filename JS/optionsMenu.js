dc4fb1b766e4b30db2b60aa2bfa2d2a396f76903
Lau Ra <newam2s.lau@gmail.com>
2022-03-25 17:43:35 +0100

    Menu déroulant depuis le js.

--- JS/optionsMenu.js
+++ JS/optionsMenu.js
@@ -1,1 +1,21 @@
 console.log("hello world");
+let detectCheatMod=document.querySelector(".input--cheat");
+let menuCheatMod=document.querySelector(".cheatModMenu");
+// if(detectCheatMod.checked==true){
+//     console.log("soir")
+// }
+
+function optionsMenu(){console.log(menuCheatMod.className);
+    if(menuCheatMod.className="inactiveted"){
+        menuCheatMod.classList.remove("inactiveted");
+        menuCheatMod.classList.add("activeOptions");
+    }else if (menuCheatMod.className="activeOptions"){
+        menuCheatMod.classList.remove("activeOptions");
+        menuCheatMod.classList.add("inactiveted");
+        
+        console.log(menuCheatMod.className);
+    }
+    
+}
+
+detectCheatMod.addEventListener("click", optionsMenu);
