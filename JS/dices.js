cc5adbe64e45c7f2075aab5f2484440c6e959aa9
Lau Ra <newam2s.lau@gmail.com>
2022-04-01 13:13:59 +0200

    Valeurs par défaut ajouté aux bonus et malus. Calcul des bonus et malus ajouter à l'affichage. Default values added to "bonus" and "malus" in HTML. Calculation of bonuses and penalities have been added.

--- JS/dices.js
+++ JS/dices.js
@@ -1,7 +1,13 @@
 let btnDice=document.querySelectorAll('.btn--dice');//récupération de TOUS les boutons de Des
 let displayDice=document.querySelector('.draw--display');//récupération de l'image du dés par défault;
 
+let displayBasicResult=document.querySelector('.numberDrawNull');//récupére le ? par default à gauche, résultat du lancer de des.
+let displayAddResult=document.querySelector('.numberResultNull');//récupère le ? par défault à droite, celui-ci sera le résultat après calcul des options
+
 let btnDraw=document.querySelector(".draw--btn");
+
+
+
 
 let convFace;//servira à convertir la valeur récupérer du des en nombre
 const min=1;
@@ -11,8 +17,15 @@ for(let dice of btnDice){//Pour chaque des de boutonDes
 }
 
 btnDraw.addEventListener("click",drawDices);
-function drawDices(){
-    if(convFace!==undefined){
+
+function getDice(){//récupérer la valeur du data et convertie en nombre 
+    let faceDice=this.dataset.dice; 
+    convFace=parseInt(faceDice);
+    displayDice.src="Assets/Des/DES"+faceDice+".png";
+    return convFace;
+}
+function drawDices(){//lancer de des
+    if(convFace!==undefined){//vérification d'une valeur
       randomThrow(min,convFace);
        
     }else{
@@ -20,13 +33,33 @@ function drawDices(){
     }
 }
 
+
+//
 function randomThrow(min,convFace){
   let basicResult=Math.floor(Math.random()*(convFace-min)+min);
-  console.log(basicResult);
+  displayBasicResult.innerHTML=basicResult;//afiche le résultat du des
+//--------RECUPERATION DES  OPTIONS--------//
+      let bonusCheck=document.querySelector('.input--bonus').value;
+        let bonus=parseInt(bonusCheck);
+      let malusCheck=document.querySelector('.input--malus').value;
+        let malus=parseInt(malusCheck);
+//--------FIN RECUPERATION DES OPTIONS--------//
+
+  checkOptions(bonusCheck,malusCheck,basicResult);
 }
-function getDice(){//récupérer la valeur du data et convertie en nombre 
-    let faceDice=this.dataset.dice; 
-    convFace=parseInt(faceDice);
-    displayDice.src="Assets/Des/DES"+faceDice+".png";
-    return convFace;
+function checkOptions(bonus,malus,basicResult){
+  if(bonus|| malus !==0){
+    let optionsResult;
+      if (bonus>0){
+        optionsResult=basicResult+bonus;
+        displayAddResult.innerHTML=optionsResult;//affiche le résultat après calcul des options
+      } 
+      else if(malus>0){
+         optionsResult=basicResult-malus;
+        displayAddResult.innerHTML=optionsResult;//affiche le résultat après calcul des options
+      }
+  }
+  else{
+     displayAddResult.innerHTML=basicResult;
+  }
 }
--- JS/dist/dices.dev.js
+++ JS/dist/dices.dev.js
@@ -4,6 +4,10 @@ var btnDice = document.querySelectorAll('.btn--dice'); //récupération de TOUS 
 
 var displayDice = document.querySelector('.draw--display'); //récupération de l'image du dés par défault;
 
+var displayBasicResult = document.querySelector('.numberDrawNull'); //récupére le ? par default à gauche, résultat du lancer de des.
+
+var displayAddResult = document.querySelector('.numberResultNull'); //récupère le ? par défault à droite, celui-ci sera le résultat après calcul des options
+
 var btnDraw = document.querySelector(".draw--btn");
 var convFace; //servira à convertir la valeur récupérer du des en nombre
 
@@ -35,23 +39,50 @@ try {
 
 btnDraw.addEventListener("click", drawDices);
 
-function drawDices() {
-  if (convFace !== undefined) {
-    randomThrow(min, convFace);
-  } else {
-    console.log("Aucun dès séléctionné");
-  }
-}
-
-function randomThrow(min, convFace) {
-  var basicResult = Math.floor(Math.random() * (convFace - min) + min);
-  console.log(basicResult);
-}
-
 function getDice() {
   //récupérer la valeur du data et convertie en nombre 
   var faceDice = this.dataset.dice;
   convFace = parseInt(faceDice);
   displayDice.src = "Assets/Des/DES" + faceDice + ".png";
   return convFace;
+}
+
+function drawDices() {
+  //lancer de des
+  if (convFace !== undefined) {
+    //vérification d'une valeur
+    randomThrow(min, convFace);
+  } else {
+    console.log("Aucun dès séléctionné");
+  }
+} //
+
+
+function randomThrow(min, convFace) {
+  var basicResult = Math.floor(Math.random() * (convFace - min) + min);
+  displayBasicResult.innerHTML = basicResult; //afiche le résultat du des
+  //--------RECUPERATION DES  OPTIONS--------//
+
+  var bonusCheck = document.querySelector('.input--bonus').value;
+  var bonus = parseInt(bonusCheck);
+  var malusCheck = document.querySelector('.input--malus').value;
+  var malus = parseInt(malusCheck); //--------FIN RECUPERATION DES OPTIONS--------//
+
+  checkOptions(bonusCheck, malusCheck, basicResult);
+}
+
+function checkOptions(bonus, malus, basicResult) {
+  if (bonus || malus !== 0) {
+    var optionsResult;
+
+    if (bonus > 0) {
+      optionsResult = basicResult + bonus;
+      displayAddResult.innerHTML = optionsResult; //affiche le résultat après calcul des options
+    } else if (malus > 0) {
+      optionsResult = basicResult - malus;
+      displayAddResult.innerHTML = optionsResult; //affiche le résultat après calcul des options
+    }
+  } else {
+    displayAddResult.innerHTML = basicResult;
+  }
 }
