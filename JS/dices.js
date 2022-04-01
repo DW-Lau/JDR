6989557278bca255a69977637f5fb4b1ce0b9a3a
Lau Ra <newam2s.lau@gmail.com>
2022-04-01 10:34:10 +0200

    Dataset HTML modifié- Récupération, lancer et générateur de nombre créer JS. Dataset's name have been changed. Value, throw and random number have been implimented to JS files
    Modification des noms dataset HTML. Récupération des valeurs des dès en fonction du clic utilisateur. Création des fonctions pour le lancer de dès (après vérification d'une valeur existante) et d'un résultat aléatoire.
    Dataset's names have been modified. Function to get back the value of a certain dice. Functions to throw dice and get a random number have been added.

--- JS/dices.js
+++ JS/dices.js
@@ -1,12 +1,32 @@
-//récupération des variables//
-let diceType=document.querySelectorAll('.btn--dice')
-// diceType.addEventListener('click',function(){
-   
-// });
-diceType.forEach(dice => {
-    dice.addEventListener('click',() => {
-        // console.log(diceType.data-dice-face);
-        let value=diceType.data;
-    console.log( value);
-    })
-})
+let btnDice=document.querySelectorAll('.btn--dice');//récupération de TOUS les boutons de Des
+let displayDice=document.querySelector('.draw--display');//récupération de l'image du dés par défault;
+
+let btnDraw=document.querySelector(".draw--btn");
+
+let convFace;//servira à convertir la valeur récupérer du des en nombre
+const min=1;
+
+for(let dice of btnDice){//Pour chaque des de boutonDes
+    dice.addEventListener('click', getDice);
+}
+
+btnDraw.addEventListener("click",drawDices);
+function drawDices(){
+    if(convFace!==undefined){
+      randomThrow(min,convFace);
+       
+    }else{
+        console.log("Aucun dès séléctionné");
+    }
+}
+
+function randomThrow(min,convFace){
+  let basicResult=Math.floor(Math.random()*(convFace-min)+min);
+  console.log(basicResult);
+}
+function getDice(){//récupérer la valeur du data et convertie en nombre 
+    let faceDice=this.dataset.dice; 
+    convFace=parseInt(faceDice);
+    displayDice.src="Assets/Des/DES"+faceDice+".png";
+    return convFace;
+}
--- JS/dist/dices.dev.js
+++ JS/dist/dices.dev.js
@@ -1,13 +1,57 @@
 "use strict";
 
-//récupération des variables//
-var diceType = document.querySelectorAll('.btn--dice'); // diceType.addEventListener('click',function(){
-// });
+var btnDice = document.querySelectorAll('.btn--dice'); //récupération de TOUS les boutons de Des
 
-diceType.forEach(function (dice) {
-  dice.addEventListener('click', function () {
-    // console.log(diceType.data-dice-face);
-    var value = diceType.data;
-    console.log(value);
-  });
-});
+var displayDice = document.querySelector('.draw--display'); //récupération de l'image du dés par défault;
+
+var btnDraw = document.querySelector(".draw--btn");
+var convFace; //servira à convertir la valeur récupérer du des en nombre
+
+var min = 1;
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = btnDice[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    var dice = _step.value;
+    //Pour chaque des de boutonDes
+    dice.addEventListener('click', getDice);
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+      _iterator["return"]();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
+}
+
+btnDraw.addEventListener("click", drawDices);
+
+function drawDices() {
+  if (convFace !== undefined) {
+    randomThrow(min, convFace);
+  } else {
+    console.log("Aucun dès séléctionné");
+  }
+}
+
+function randomThrow(min, convFace) {
+  var basicResult = Math.floor(Math.random() * (convFace - min) + min);
+  console.log(basicResult);
+}
+
+function getDice() {
+  //récupérer la valeur du data et convertie en nombre 
+  var faceDice = this.dataset.dice;
+  convFace = parseInt(faceDice);
+  displayDice.src = "Assets/Des/DES" + faceDice + ".png";
+  return convFace;
+}
