816cc3789dbfb8d80cc9d98c89cf6744041ab1b4
Lau Ra <newam2s.lau@gmail.com>
2022-04-13 16:19:00 +0200

    Responsiv screen Large 1441px break

--- JS/dist/optionsMenu.dev.js
+++ JS/dist/optionsMenu.dev.js
@@ -3,6 +3,11 @@
 var detectCheatMod = document.querySelector(".input--cheat");
 var menuCheatMod = document.querySelector("[data-info]"); //récupére  la div cachée "cheatModMenu"
 
+var critSucces = document.querySelector(".cheatsucces");
+var menuSucces = document.querySelector("[data-succes]");
+var critFail = document.querySelector(".cheatfail");
+var menuFail = document.querySelector("[data-fail]");
+
 function affichageMenu() {
   //modification du name class pour faire apparaître la div voulue
   var classMenu = menuCheatMod.className;
@@ -18,6 +23,8 @@ function affichageMenu() {
       menuCheatMod.classList.add("inactivated"); //désactive la div par le css
 
       menuCheatMod.classList.remove("activeOptions");
+      critSucces.classList = "cheatsucces";
+      critFail.classList = "cheatfail";
       break;
 
     default:
@@ -25,4 +32,20 @@ function affichageMenu() {
   }
 }
 
+function activeSucces() {
+  //modification du name class pour faire apparaître la div voulue
+  critSucces.classList.add("activeSucces"); //apparition de la div par le css
+
+  critSucces.classList.remove("cheatsucces");
+}
+
+function activeFail() {
+  //modification du name class pour faire apparaître la div voulue
+  critFail.classList.add("activeFail"); //apparition de la div par le css
+
+  critFail.classList.remove("cheatfail");
+}
+
 detectCheatMod.addEventListener("click", affichageMenu);
+critSucces.addEventListener("click", activeSucces);
+critFail.addEventListener("click", activeFail);
